c6c0b9cf0972e06d9baa915d3055403a04b24a36
hannyyf <hannyfauzia2@gmail.com>
2018-12-12 16:30:29 +0700

    config file dan alter data
    Signed-off-by: hannyyf <hannyfauzia2@gmail.com>

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app/tempXML'),
         ],
 
+        'local_two' => [
+            'driver' => 'local',
+            'root' => storage_path('app/resultXML'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
